9acffbc68c077e3390bd9b3ddbf7a333e37ead03
Travis Jefferson <traviscjefferson@gmail.com>
2020-07-18 19:59:27 -0400

    Some buttons and some SICK UX

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 import { repl } from "./repl.js";
 
 console.log("INIT");
-repl();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form[name=repl]");
+  const prompt = document.querySelector("[name=prompt]");
+  const clearBtn = document.querySelector(".clear-btn");
+  const scrollback = document.querySelector(".scrollback");
+
+  clearBtn.addEventListener("click", () => {
+    scrollback.innerHTML = "";
+  });
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    const result = repl(prompt.value);
+    prompt.value = "";
+    const toPrint = document.createElement("div");
+    toPrint.innerText = result;
+    scrollback.appendChild(toPrint);
+  });
+});
--- js/repl.js
+++ js/repl.js
@@ -1,1 +1,5 @@
-export const repl = () => console.log("TODO: repl");
+export const repl = str => {
+  console.log("about to read str", str);
+  const evaledResult = str; // TODO: implement eval oh and read and print
+  return `[Printed]: ${evaledResult}`;
+};
